31e52c698ca299660721c1fecc91679d0c867b54
juarex9 <agustinjuarez375@gmail.com>
2026-01-21 20:01:26 -0300

    refactor: optimize connection to firebase

--- src/services/config/firebase.js
+++ src/services/config/firebase.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
- 
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPOmyunEG-lZ7_su6zwYcgK9AM_wPxG8U",
   authDomain: "coderhouse-react-89d1f.firebaseapp.com",
   projectId: "coderhouse-react-89d1f",
-  storageBucket: "coderhouse-react-89d1f.firebasestorage.app",
+  storageBucket: "coderhouse-react-89d1f.appspot.com", // <-- recomendado
   messagingSenderId: "393611698838",
   appId: "1:393611698838:web:c1c657b9d0dc8289aa18b7",
-  measurementId: "G-ZS9RG5EHKC"
+  measurementId: "G-ZS9RG5EHKC",
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
-export const db = getFirestore();
+// Si querés analytics, mejor activarlo solo en prod (y en browser)
+// export const analytics = getAnalytics(app);
