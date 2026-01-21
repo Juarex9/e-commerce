ee99258d5ad62aae0259ebe3f49aab8077bb125b
juarex9 <agustinjuarez375@gmail.com>
2026-01-21 19:59:54 -0300

    style: new style to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import ItemListContainer from '../components/ItemListContainer';
+import HomeHero from '../components/HomeHero';
 import { useProducts } from '../hooks/useProducts';
+import Footer from '../components/Footer';
+import IncentivePage from '../components/IncentivePage';
 
 const Home = () => {
     const {products, loading, error} = useProducts();
 
     ///renderizado condicional:
     if (loading) return <>Loading...</>;
-    if (error) return <>Error loading products</>;
+    if (error) return <pre>{String(error?.message ?? error)}</pre>;
 
-    return <ItemListContainer products={products}/>;
-
+    return (
+        <>
+        <HomeHero/>
+        <ItemListContainer products={products}/>;
+        <IncentivePage/>
+        <Footer/>
+        </>
+    );
 };
 
 export default Home;
